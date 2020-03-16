604dff430bd91a7b513d9aee524f7c7faf734001
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2020-03-16 16:57:57 -0400

    init

---
+++ pubsub/ps.js
@@ -0,0 +1,62 @@
+
+
+// code to create case
+async function createCase(subject, at, priority) {
+    const sobj = 'Case';
+    const url = `https://mohansun-fsc-21.my.salesforce.com/services/data/v46.0/sobjects/${sobj}`;
+   
+
+    const postbody = { "Subject": `${subject}`, "Priority": `${priority}`} ;
+
+    const params = {
+        method: "POST",
+        mode: "cors",
+        headers: {"Content-Type":"application/json", "Authorization": `Bearer ${at}` },
+        body: JSON.stringify(postbody)
+    };
+    try { await fetch(url, params)
+    }
+    catch (err) {
+      console.log('Fetch ERROR!' + err);
+    }
+}
+
+
+ const subscriptionName = 'CV19_CASE';
+ const timeout = 60;
+
+// Imports the Google Cloud client library
+const {PubSub} = require('@google-cloud/pubsub');
+
+// Creates a client; cache this for further use
+const pubSubClient = new PubSub();
+
+function listenForMessages() {
+  // References an existing subscription
+  const subscription = pubSubClient.subscription(subscriptionName);
+
+  // Create an event handler to handle messages
+  let messageCount = 0;
+  const messageHandler = message => {
+    console.log(`Received message ${message.id}:`);
+    console.log(`\tData: ${message.data}`);
+    console.log(`\tAttributes: ${message.attributes}`);
+    messageCount += 1;
+
+    // CREATE Case here
+     cons cst = await createCase(message.data.subject, message.data.at, message.data.priority) 
+
+    // "Ack" (acknowledge receipt of) the message
+    message.ack(message.data.);
+  };
+
+  // Listen for new messages until timeout is hit
+  subscription.on('message', messageHandler);
+
+  setTimeout(() => {
+    subscription.removeListener('message', messageHandler);
+    console.log(`${messageCount} message(s) received.`);
+  }, timeout * 1000);
+}
+
+listenForMessages()
